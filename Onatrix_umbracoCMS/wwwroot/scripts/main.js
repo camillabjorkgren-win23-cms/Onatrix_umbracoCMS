1d8507485ad961a0f0d6865fd2517cc0f42f0e23
Camilla Björkgren <camilla_bjorkgren@hotmail.com>
2024-09-16 10:21:22 +0200

    forms

---
+++ Onatrix_umbracoCMS/wwwroot/scripts/main.js
@@ -0,0 +1,13 @@
+function handleContactFormSubmit(event) {
+    event.preventDefault()
+
+    fetch('https://api.domain.com/contactform')
+    .then(() => {
+
+    })
+    .then(() => {
+
+    }).catch(error => {
+
+    })
+} 
