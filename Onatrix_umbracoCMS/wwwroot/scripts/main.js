ed5c1aca20913471f672d57845f47d3fcdef9fc6
Camilla Björkgren <camilla_bjorkgren@hotmail.com>
2024-09-26 09:21:21 +0200

    klara med umbraco onatrix webapp

--- Onatrix_umbracoCMS/wwwroot/scripts/main.js
+++ Onatrix_umbracoCMS/wwwroot/scripts/main.js
@@ -1,44 +1,4 @@
 
- function handleContactFormSubmit(event) {
-     event.preventDefault(); 
-
-     const name = document.getElementById('name');
-     const email = document.getElementById('email');
-     const message = document.getElementById('message');
-
-     let isValid = true; 
-
-  
-     if (name.value.trim() === '') {
-         showError(name, 'You must enter your name');
-         isValid = false;
-     } else {
-         hideError(name);
-     }
-
-
-     if (!email.validity.valid) {
-         showError(email, 'You must enter a valid email');
-         isValid = false;
-     } else {
-         hideError(email);
-     }
-
- 
-     if (message.value.trim() === '') {
-         showError(message, 'You must enter a question');
-         isValid = false;
-     } else {
-         hideError(message);
-     }
-
-
-     if (isValid) {
-         console.log('Form is valid, submit the form');
-     }
- }
-
-
  function showError(input, message) {
      const errorSpan = input.parentElement.querySelector('.form-input-error');
      errorSpan.textContent = message;
@@ -51,5 +11,11 @@
      errorSpan.style.display = 'none'; 
  }
 
+function toggleMenu() {
+    var menu = document.querySelector('.main-menu');
+    menu.classList.toggle('show');
 
+    var isExpanded = menu.classList.contains('show');
+    document.querySelector('.btn-menu').setAttribute('aria-expanded', isExpanded);
+}
 
